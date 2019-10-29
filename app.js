10e0fc3e257ad7df920f2b49f0efe995cb39ce06
Brendon Hampton <brendonhampton1988@gmail.com>
2019-10-29 13:18:08 -0700

    Added borders, added background color, Centered all content, added footer contact info, changed font style for page

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 "use Strict"
 
+// does the author work for a shipping company?
+// did the author enlist in 2009?
+// true or false : Brendon was dishonerably discharged
+// true or false : The Author completed 3 quarters of Community College
+// Has the author ever been employed as a security officer?
+
