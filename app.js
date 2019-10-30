c97b79af8dbe208851c587377a9011a5d0a873ce
Brendon Hampton <brendonhampton1988@gmail.com>
2019-10-29 20:49:57 -0700

    Removed and changed some of the javascript questions

--- app.js
+++ app.js
@@ -6,22 +6,15 @@
 // true or false : The Author completed 3 quarters of Community College
 // Has the author ever been employed as a security officer?
 
-var workHistory = prompt("Does the Author work for a shipping company?"); {
-    if(workHistory == "yes") {
-        alert("You are wrong");
+var workHistory = prompt("Does the Author work for a shipping company?");
+    if(workHistory.toLowerCase() == "no") {
+        alert("good Job!");
     }
-    else(alert("That is right! Good Job!"));
-}
 
-var militaryService = prompt("Did the author enlist in 2009?"); {
-    if(militaryService =="yes") {
-        alert("That is correct");
-    }
-    else(alert("OOOH too bad your wrong"));
-}
+// var militaryService = prompt("Did the author enlist in 2009?"); 
 
-var disCharged == prompt("True or False : Brendon was Dishonerably Discharged?"); {
-    if(disCharged === true || "true") {
-        alert("That is actually false! You fool!");
-    }
-}
+// var disCharged = prompt("True or False : Brendon was Dishonerably Discharged?"); 
+
+// var completeCollege = prompt("True or false : The author completed 3 quarters of Community College"); 
+
+// var employedAs = prompt("Has the author ever been employed as a security officer?"); 
