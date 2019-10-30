b2250ffcf68a3711fcb3cb4601f0bc9f4060b1e9
Brendon Hampton <brendonhampton1988@gmail.com>
2019-10-29 23:51:30 -0700

    Added Exit message to function, Rounded image border, added margin to button, added console.log, finished welcomeMsg variable on startup of page

--- app.js
+++ app.js
@@ -6,13 +6,16 @@
 // true or false : The Author completed 3 quarters of Community College
 // Has the author ever been employed as a security officer?
 
-var personalMessage = alert("Hello! Welcome to my site! Please take the test after reading my Bio to get to know me better!");
-var userName = prompt("What is your name?"); 
+
+var welcomeMsg = prompt("What is your name?"); { 
+    alert("Hello! Welcome to my site " + welcomeMsg + "! Please take the test after reading my Bio to get to know me better!");
+    }
 
 function bioTest() {
     var workHistory = prompt("Does the Author work for a shipping company?"); {
         if(workHistory.toLowerCase() == "no") {
             alert("good Job!");
+            // console.log("That is the right answer!");
         }
         else(alert("Yeah thats wrong!"));
     }
@@ -20,6 +23,7 @@ function bioTest() {
     var militaryService = prompt("Did the author enlist in 2009?"); {
         if(militaryService.toUpperCase() == "YES") {
             alert("Yes thats Correct!");
+            // console.log("That is correct!");
         }
         else(alert("Nope!Nada!Wrongo!"));
     }
@@ -27,6 +31,7 @@ function bioTest() {
     var disCharged = prompt("True or False : Brendon was Dishonerably Discharged?"); {
         if(disCharged.toLowerCase() == "false") {
             alert("Thats Correct again!");
+            // console.log("That is right!");
         }
         else(alert("yeah...thats wrong!"));
     }
@@ -34,14 +39,17 @@ function bioTest() {
     var completeCollege = prompt("True or false : The author completed 3 quarters of Community College"); {
         if(completeCollege.toLowerCase() == "false") {
             alert("You got it!!");
+            // console.log("That is the answer!");
         }
         else(alert("yeah...your not even trying!"));
     }
 
     var employedAs = prompt("Has the author ever been employed as a security officer?"); {
         if(employedAs.toUpperCase() == "YES") {
-            alert("Congrats you payed attention to something!");
+            alert("Congrats! you payed attention to something!");
+            // console.log("That is the correct answer!");
         }
         else(alert("Congrats! You didnt pay attention!"));
     }
+    var exitMsg = (alert("It was nice meeting you " + welcomeMsg + " Thank you for visiting my page and learning alittle about me!"));
 }
