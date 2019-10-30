f0aa9e5cf1f9c4ef77a10d41d0c74a7a7a3af539
Brendon Hampton <brendonhampton1988@gmail.com>
2019-10-29 21:17:40 -0700

    Added Javascript questionairre, Added button to call function

--- app.js
+++ app.js
@@ -6,15 +6,39 @@
 // true or false : The Author completed 3 quarters of Community College
 // Has the author ever been employed as a security officer?
 
-var workHistory = prompt("Does the Author work for a shipping company?");
-    if(workHistory.toLowerCase() == "no") {
-        alert("good Job!");
+function bioTest() {
+    var workHistory = prompt("Does the Author work for a shipping company?"); {
+        if(workHistory.toLowerCase() == "no") {
+            alert("good Job!");
+        }
+        else(alert("Yeah thats wrong!"));
     }
 
-// var militaryService = prompt("Did the author enlist in 2009?"); 
+    var militaryService = prompt("Did the author enlist in 2009?"); {
+        if(militaryService.toUpperCase() == "YES") {
+            alert("Yes thats Correct!");
+        }
+        else(alert("Nope!Nada!Wrongo!"));
+    }
 
-// var disCharged = prompt("True or False : Brendon was Dishonerably Discharged?"); 
+    var disCharged = prompt("True or False : Brendon was Dishonerably Discharged?"); {
+        if(disCharged.toLowerCase() == "false") {
+            alert("Thats Correct again!");
+        }
+        else(alert("yeah...thats wrong!"));
+    }
 
-// var completeCollege = prompt("True or false : The author completed 3 quarters of Community College"); 
+    var completeCollege = prompt("True or false : The author completed 3 quarters of Community College"); {
+        if(completeCollege.toLowerCase() == "false") {
+            alert("You got it!!");
+        }
+        else(alert("yeah...your not even trying!"));
+    }
 
-// var employedAs = prompt("Has the author ever been employed as a security officer?"); 
+    var employedAs = prompt("Has the author ever been employed as a security officer?"); {
+        if(employedAs.toUpperCase() == "YES") {
+            alert("Congrats you payed attention to something!");
+        }
+        else(alert("Congrats! You didnt pay attention!"));
+    }
+}
