f78c81f22a3e69bc81686860b29c229d283ecf36
Brendon Hampton <brendonhampton1988@gmail.com>
2019-10-29 18:58:51 -0700

    Added 3 JS questions

--- app.js
+++ app.js
@@ -6,3 +6,22 @@
 // true or false : The Author completed 3 quarters of Community College
 // Has the author ever been employed as a security officer?
 
+var workHistory = prompt("Does the Author work for a shipping company?"); {
+    if(workHistory == "yes") {
+        alert("You are wrong");
+    }
+    else(alert("That is right! Good Job!"));
+}
+
+var militaryService = prompt("Did the author enlist in 2009?"); {
+    if(militaryService =="yes") {
+        alert("That is correct");
+    }
+    else(alert("OOOH too bad your wrong"));
+}
+
+var disCharged == prompt("True or False : Brendon was Dishonerably Discharged?"); {
+    if(disCharged === true || "true") {
+        alert("That is actually false! You fool!");
+    }
+}
