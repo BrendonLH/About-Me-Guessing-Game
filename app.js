3d561c770bcc11b197945590a6bd72c8b26db4b7
Brendon Hampton <brendonhampton1988@gmail.com>
2019-10-31 19:55:20 -0700

    Changed margin of articles, Added work experience and Education

--- app.js
+++ app.js
@@ -7,49 +7,50 @@
 // Has the author ever been employed as a security officer?
 
 
-var welcomeMsg = prompt('What is your name?'); {
-  alert('Hello! Welcome to my site ' + welcomeMsg + '! Please take the test after reading my Bio to get to know me better!');
-}
+// var welcomeMsg = prompt('What is your name?'); {
+//   alert('Hello! Welcome to my site ' + welcomeMsg + '! Please take the test after reading my Bio to get to know me better!');
+// }
 
-function bioTest() {
-  var workHistory = prompt('Does the Author work for a shipping company?'); {
-    if(workHistory.toLowerCase() === 'no') {
-      alert('good Job!');
-      // console.log("That is the right answer!");
-    }
-    else(alert('Yeah thats wrong!'));
-  }
+// function bioTest() {
+//   var workHistory = prompt('Does the Author work for a shipping company?'); {
+//     if(workHistory.toLowerCase() === 'no') {
+//       alert('good Job!');
+//       // console.log("That is the right answer!");
+//     }
+//     else(alert('Yeah thats wrong!'));
+//   }
 
-  var militaryService = prompt('Did the author enlist in 2009?'); {
-    if(militaryService.toUpperCase() === 'YES') {
-      alert('Yes thats Correct!');
-      // console.log("That is correct!");
-    }
-    else(alert('Nope!Nada!Wrongo!'));
-  }
+//   var militaryService = prompt('Did the author enlist in 2009?'); {
+//     if(militaryService.toUpperCase() === 'YES') {
+//       alert('Yes thats Correct!');
+//       // console.log("That is correct!");
+//     }
+//     else(alert('Nope!Nada!Wrongo!'));
+//   }
 
-  var disCharged = prompt('True or False : Brendon was Dishonerably Discharged?'); {
-    if(disCharged.toLowerCase() === 'false') {
-      alert('Thats Correct again!');
-      // console.log("That is right!");
-    }
-    else(alert('yeah...thats wrong!'));
-  }
+//   var disCharged = prompt('True or False : Brendon was Dishonerably Discharged?'); {
+//     if(disCharged.toLowerCase() === 'false') {
+//       alert('Thats Correct again!');
+//       // console.log("That is right!");
+//     }
+//     else(alert('yeah...thats wrong!'));
+//   }
 
-  var completeCollege = prompt('True or false : The author completed 3 quarters of Community College'); {
-    if(completeCollege.toLowerCase() === 'false') {
-      alert('You got it!!');
-      // console.log("That is the answer!");
-    }
-    else(alert('yeah...your not even trying!'));
-  }
+//   var completeCollege = prompt('True or false : The author completed 3 quarters of Community College'); {
+//     if(completeCollege.toLowerCase() === 'false') {
+//       alert('You got it!!');
+//       // console.log("That is the answer!");
+//     }
+//     else(alert('yeah...your not even trying!'));
+//   }
 
-  var employedAs = prompt('Has the author ever been employed as a security officer?'); {
-    if(employedAs.toUpperCase() === 'YES') {
-      alert('Congrats! you payed attention to something!');
-      // console.log("That is the correct answer!");
-    }
-    else(alert('Congrats! You didnt pay attention!'));
-  }
-  var exitMsg = (alert('It was nice meeting you ' + welcomeMsg + ' Thank you for visiting my page and learning alittle about me!'));
-}
+//   var employedAs = prompt('Has the author ever been employed as a security officer?'); {
+//     if(employedAs.toUpperCase() === 'YES') {
+//       alert('Congrats! you payed attention to something!');
+//       // console.log("That is the correct answer!");
+//     }
+//     else(alert('Congrats! You didnt pay attention!'));
+//   }
+//   var exitMsg = (alert('It was nice meeting you ' + welcomeMsg + ' Thank you for visiting my page and learning alittle about me!'));
+// }
+
